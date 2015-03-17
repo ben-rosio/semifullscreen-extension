8ab0adb0d2c96f82a5f7383dc61fbaed7326004b
ben <ben@ros.io>
2015-03-16 23:48:22 -0600

    Add more tests for Utility.contain

--- spec/embedded.js
+++ spec/embedded.js
@@ -4,5 +4,9 @@ describe("Utilities", function() {
         expect(SemiscreenExtension.Utilities.contain([50, 100], [50, 50])).toEqual([25, 50]);
 
         expect(SemiscreenExtension.Utilities.contain([100, 50], [55, 55], [5, 5])).toEqual([50, 25]);
+        expect(SemiscreenExtension.Utilities.contain([50, 200], [100, 100])).toEqual([25, 100]);
+
+        expect(SemiscreenExtension.Utilities.contain([50, 25], [100, 200])).toEqual([100, 50]);
+        expect(SemiscreenExtension.Utilities.contain([25, 50], [200, 100])).toEqual([50, 100]);
     });
 });
